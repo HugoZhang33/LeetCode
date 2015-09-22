0b52736b00a9ef96eb9ba02fac5179e63328dc7b
HugoZhang33 <zhanghong.gdufs@gmail.com>
2015-09-22 15:00:56 -0400

    add new solution

--- Java/Different Ways to Add Parentheses.java
+++ Java/Different Ways to Add Parentheses.java
@@ -66,3 +66,59 @@ public class Solution {
         return res;
     }
 }
+
+// solution 2
+public class Solution {
+    public List<Integer> diffWaysToCompute(String input) {
+        if (input == null || input.length() == 0)
+            return new ArrayList<Integer>();
+        
+        List<String> strs = new ArrayList<String>();
+        String s = "";
+        for (int i=0; i<input.length(); i++) {
+            if (Character.isDigit(input.charAt(i))) {
+                s += input.charAt(i);
+            } else {
+                strs.add(s);
+                s = "";
+                strs.add(input.charAt(i)+"");
+            }
+        }
+        strs.add(s);
+        
+        return diffWaysToCompute(strs);
+    }
+    
+    private List<Integer> diffWaysToCompute(List<String> strs) {
+        List<Integer> res = new ArrayList<Integer>();
+        if (strs.size() == 1) {
+            res.add(Integer.parseInt(strs.get(0)));
+            return res;
+        }
+        for (int i=0; i<strs.size(); i+=2) {
+            if (i+1 >= strs.size()) continue;
+            List<Integer> la = diffWaysToCompute(strs.subList(0, i+1));
+            char op = strs.get(i+1).charAt(0);
+            List<Integer> lb = diffWaysToCompute(strs.subList(i+2, strs.size()));
+            for (int a : la) {
+                for (int b : lb) {
+                    switch (op) {
+                        case '+':
+                            res.add(a+b);
+                            break;
+                        case '-':
+                            res.add(a-b);
+                            break;
+                        case '*':
+                            res.add(a*b);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
+
